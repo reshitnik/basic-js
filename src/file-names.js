ac524f534b62eed8311e748e080b870b8b95f1a3
reshitnik <reshitnik@gmail.com>
2023-12-17 22:23:59 +0300

    task9-10

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let arr = [];
+  for (i = 0; i < names.length; i++){
+      if (!arr.includes(names[i])){
+          arr.push(names[i])
+      } else{
+         let k = 1
+          while(true){
+              if (arr.includes(`${names[i]}(${k})`)){
+                  k++
+              }else{
+                  arr.push(`${names[i]}(${k})`)
+                  break
+              }
+          }
+      }
+  }
+  return arr
 }
 
 module.exports = {
--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  const arr = ['.', ',', '-', '!', '^', '<', '>', '%'];
+  let str_index = email.indexOf('@');
+  if (!arr.includes(email[str_index+1])) {
+      return email.slice(str_index+1, email.length)
+  } else {
+      newEmail = email.slice(str_index+1, email.length)
+      return getEmailDomain(newEmail)
+  }
 }
 
 module.exports = {
