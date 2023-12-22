2a58979021f495654c27b332977924695122b215
reshitnik <reshitnik@gmail.com>
2023-12-22 23:59:10 +0300

    task19

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform (arr) {
+  let a;
+  for (i = 0; i<arr.length; i++){
+      if (arr[i] === '--double-next'){
+          a = arr[i+1]
+          arr = [...arr.splice(0,i),...[a],...arr.splice(1,arr.length)]
+      } 
+      if (arr[i] === '--double-prev'){
+          a = arr[i-1]
+          arr = [...arr.splice(0,i),...[a],...arr.splice(1,arr.length)]
+      }
+      if (arr[i] === '--discard-prev'){
+          arr = [...arr.splice(0,i-1),...arr.splice(2,arr.length)]
+      }
+      if (arr[i] === '--discard-next'){
+          arr = [...arr.splice(0,i),...arr.splice(2,arr.length)]
+      }
+}
+      return arr.filter(a => typeof a =='number')
 }
 
 module.exports = {
